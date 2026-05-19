18b34a008acdecfd4217273cc34db09753e6e160
vivolikea <97621657+vivolikea@users.noreply.github.com>
2026-05-19 09:58:17 +0000

    prevent email duplication

--- src/app.py
+++ src/app.py
@@ -62,6 +62,9 @@ def signup_for_activity(activity_name: str, email: str):
     # Get the specific activity
     activity = activities[activity_name]
 
-    # Add student
+    # Prevent duplicate signups
+    if email in activity["participants"]:
+        raise HTTPException(status_code=400, detail="Student already signed up")
+
     activity["participants"].append(email)
     return {"message": f"Signed up {email} for {activity_name}"}
